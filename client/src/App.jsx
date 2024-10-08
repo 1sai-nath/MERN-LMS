14c49f1632b47d4ad1a6c50fbebb46dc804827cc
sai-nath <ssjawadwar@gmail.com>
2024-10-08 15:10:53 +0530

    created logo and signin signup buttons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Button } from './components/ui/button'
+
+import { Route, Routes } from 'react-router-dom'
+import AuthPage from './pages/auth'
 
 const App = () => {
   return (
     <div>
-      <Button className="bg-red-600">check</Button>
+      <Routes>
+        <Route path='/auth' element={<AuthPage/>}/>
+      </Routes>
     </div>
   )
 }
